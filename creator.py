53fc4e3cdf05b9780bbe6b85cfa800ae40af0eb1
evro <57731147+evro@users.noreply.github.com>
2020-03-03 17:25:57 -0500

    Create creator.py
    If you get timed-out by Krunker with 'Request Timed Out' 
    
    make a new chrome profile

---
+++ creator.py
@@ -0,0 +1,22 @@
+    from selenium import webdriver
+import time
+
+url = ('https://krunker.io')
+
+driver = webdriver.Chrome('/Users/Win10v370.PLCHNET/Downloads/chromedriver')
+driver.get(url)
+
+driver.find_element_by_class_name("Register").click()
+
+time.sleep(2)
+
+///if you want to make more random accs, just put whatever you want, then put a comma and put whatever you want again. :)
+
+driver.find_element_by_id('accName').send_keys('YOURTEXTHERE')
+driver.find_element_by_id('accPass').send_keys('YOURTEXTHERE')
+
+
+driver.find_element_by_class_name("accountButton").click()
+
+end
+end
